8c3849f2513287fdb167795d8f8591c697a4e5e8
lh6104 <longha6104@gmail.com>
2023-11-14 02:30:17 +0700

    command line ver

---
+++ base/Main.java
@@ -0,0 +1,11 @@
+package base;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        DictionaryCommandline write = new DictionaryCommandline();
+        write.dictionaryAdvanced();
+    }
+}
